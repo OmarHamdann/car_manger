df11c0be872bb6f59e1c2f6e075ed87edcd9425d
OmarHamdann <omar.haamdan@gmail.com>
2022-07-27 17:23:55 +0300

    car mock

--- src/car/cars.mock.ts
+++ src/car/cars.mock.ts
@@ -1,46 +1,26 @@
-export const CARS=[
-    {
-        id:1,
-        name:'BMW',
-        price:30000,
-        year:2010,
-        color:'red',
-        description:'BMW is a German automobile manufacturer',
-        isSold:false,
-        createdAt:new Date(),
-        updatedAt:new Date(),
-    },
-    {
-        id:2,
-        name:'Audi',
-        price:40000,
-        year:2011,
-        color:'blue',
-        description:'Audi is a German automobile manufacturer',
-        isSold:false,
-        createdAt:new Date(),
-        updatedAt:new Date(),
-    },
-    {
-        id:3,
-        name:'Mercedes',
-        price:50000,
-        year:2012,
-        color:'black',
-        description:'Mercedes is a German automobile manufacturer',
-        isSold:false,
-        createdAt:new Date(),
-        updatedAt:new Date(),
-    },
-    {
-        id:4,
-        name:'BMW',
-        price:30000,
-        year:2010,
-        color:'red',
-        description:'BMW is a German automobile manufacturer',
-        isSold:false,
-        createdAt:new Date(),
-        updatedAt:new Date(),
-    }
-]
+export const CARS = [
+  {
+    id: 1,
+    name: 'BMW',
+    price: '30000',
+    color: 'red',
+  },
+  {
+    id: 2,
+    name: 'Audi',
+    price: '40000',
+    color: 'blue',
+  },
+  {
+    id: 3,
+    name: 'Mercedes',
+    price: '50000',
+    color: 'black',
+  },
+  {
+    id: 4,
+    name: 'BMW',
+    price: '30000',
+    color: 'red',
+  },
+];
