8be4b2f68f61c220beabd7cf0aa1ed4611e2d80d
OmarHamdann <omar.haamdan@gmail.com>
2022-07-28 16:48:48 +0300

    connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
-
 import { CarModule } from './car/car.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [CarModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://root:root@cluster0.8zwnzz4.mongodb.net/?retryWrites=true&w=majority'),
+    CarModule],
 })
 export class AppModule {}
