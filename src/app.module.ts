26150504f93edbc5d60a90e4e0ef467de81c9c6d
OmarHamdann <omar.haamdan@gmail.com>
2022-08-14 16:08:56 +0300

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { CarModule } from './car/car.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
+//this is the module for car
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://root:root@cluster0.8zwnzz4.mongodb.net/?retryWrites=true&w=majority'),
