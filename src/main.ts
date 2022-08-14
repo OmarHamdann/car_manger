1e5fb3545678b98ca561fc742b8a9574bf30db44
OmarHamdann <omar.haamdan@gmail.com>
2022-08-14 16:09:03 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
+//this is the main function
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+//this is invoke the main function
 bootstrap();
